51483ec6e3696303920ec465b54e62388cc0a37b
Long <thdailong@gmail.com>
2023-10-23 01:48:51 +0700

    Add guard

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateOrderDto } from './create_order.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('order')
 export class OrderController {
   constructor(private orderService: OrderService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   @ApiTags('order')
   async getAllOrders() {
@@ -17,5 +19,18 @@ export class OrderController {
   @ApiTags('order')
   async createOrder(@Body() order: CreateOrderDto) {
     return this.orderService.create(order);
+  }
+
+  @Get(':phone')
+  @ApiTags('order')
+  async getOrderByPhone(@Param('phone') phone: string) {
+    return this.orderService.findOrderByPhone(phone);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/status')
+  @ApiTags('order')
+  async updateStatusOrder(@Param('id') id: string, @Body() status: string) {
+    return this.orderService.updateStatusOrder(id, status);
   }
 }
