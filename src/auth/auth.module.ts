66aebcf839c95d6efa18dd1b945d9c39762af218
Long <thdailong@gmail.com>
2023-10-30 13:44:19 +0700

    Upate

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from './constants';
       global: true,
       secret: jwtConstants.secret,
       signOptions: {
-        expiresIn: '3600s',
+        expiresIn: '1d',
       },
     }),
   ],
